bae5f9af71a8f671208612744aa809644f240b09
Tobias Belch <tobias.belch@docmorris.de>
2025-08-11 13:39:23 +0200

    fix: deployment

--- apps/amelcraft/vite.config.ts
+++ apps/amelcraft/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
+  base: process.env.NODE_ENV === "production" ? "/bd-25/amelcraft/" : "/",
   build: {
     outDir: "dist",
   },
