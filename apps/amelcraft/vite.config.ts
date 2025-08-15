f8b08fa5c3decf58cc8d8c75a17a523a5e280645
Tobias Belch <tobias.belch@docmorris.de>
2025-08-15 17:28:41 +0200

    chore: run build before preview

--- apps/amelcraft/vite.config.ts
+++ apps/amelcraft/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: ".",
-  base: process.env.NODE_ENV === "production" ? "/bd-25/amelcraft/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/bd-25/" : "/",
   build: {
     outDir: "dist",
   },
