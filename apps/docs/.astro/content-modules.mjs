8e5995f8e15b7230d0ec41bdf883e4ea99d456ef
Tobias Belch <tobias.belch@docmorris.de>
2025-09-07 11:00:43 +0200

    docs: assets

--- apps/docs/.astro/content-modules.mjs
+++ apps/docs/.astro/content-modules.mjs
@@ -1,4 +1,5 @@
 
 export default new Map([
-["../../docs/CONCEPT.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2F..%2Fdocs%2FCONCEPT.mdx&astroContentModuleFlag=true")]]);
+["../../docs/CONCEPT.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2F..%2Fdocs%2FCONCEPT.mdx&astroContentModuleFlag=true")],
+["../../docs/assets/README.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=..%2F..%2Fdocs%2Fassets%2FREADME.mdx&astroContentModuleFlag=true")]]);
 		
